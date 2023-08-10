4a9697309da1c5f2229ccef8d258b4d9c3825a51
reyfacundo <reyfacundotobias@gmail.com>
2023-08-10 02:42:59 -0300

    fix

--- public/js/index.js
+++ public/js/index.js
@@ -2,47 +2,47 @@ const socket = io();
 
 let user;
 
-let chatBox = document.getElementById("chatBox");
+let chatbox = document.getElementById("chatbox");
 
 Swal.fire({
-  title: "Inicia sesion!",
-  text: "Ingresa tu nombre de usuario",
-  input: "text",
-  confirmButtonText: "Cool",
-  allowOutsideClick: false,
-  inputValidator: (value) => {
-    if (!value) {
-      return "Debe ingresar un nombre de usuario";
-    }
-  },
+    title: "Inicia sesion!",
+    text: "Ingresa tu nombre de usuario",
+    input: "text",
+    confirmButtonText: "Cool",
+    allowOutsideClick: false,
+    inputValidator: (value) => {
+        if (!value) {
+            return "Debe ingresar un nombre de usuario";
+        }
+    },
 }).then((result) => {
-  if (result.value) {
-    user = result.value;
-    socket.emit("new-user", { user: user, id: socket.id });
-  }
+    if (result.value) {
+        user = result.value;
+        socket.emit("new-user", { user: user, id: socket.id });
+    }
 });
 
-chatBox.addEventListener("keyup", (e) => {
-  console.log("chatBox existe", chatBox);
-  console.log("evento key up", e.key, chatBox.value);
-  if (e.key === "Enter") {
-    if (chatBox.value.trim().length > 0) {
-      socket.emit("message", {
-        user: user,
-        message: chatBox.value,
-      });
-      chatBox.value = "";
+chatbox.addEventListener("keyup", (e) => {
+    console.log("chatBox existe", chatbox);
+    console.log("evento key up", e.key, chatbox.value);
+    if (e.key === "Enter") {
+        if (chatbox.value.trim().length > 0) {
+            socket.emit("message", {
+                user: user,
+                message: chatbox.value,
+            });
+            chatbox.value = "";
+        }
     }
-  }
 });
 
 socket.on("messageLogs", (data) => {
-  let log = document.getElementById("messageLogs");
-  let message = "";
+    let log = document.getElementById("messageLogs");
+    let message = "";
 
-  data.forEach((elem) => {
-    message += `
-     
+    data.forEach((elem) => {
+        message += `
+    
         <div class="chat-message">
         <div class="message-bubble">
   
@@ -52,16 +52,16 @@ socket.on("messageLogs", (data) => {
   
         </div>
       `;
-  });
+    });
 
-  log.innerHTML = message;
+    log.innerHTML = message;
 });
 
 socket.on("new-user-connected", (data) => {
-  if (data.id !== socket.id)
-    Swal.fire({
-      text: `${data.user} se ha conectado al chat`,
-      toast: true,
-      position: "top-end",
-    });
+    if (data.id !== socket.id)
+        Swal.fire({
+            text: `${data.user} se ha conectado al chat`,
+            toast: true,
+            position: "top-end",
+        });
 });
